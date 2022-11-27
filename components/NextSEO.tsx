1342e3ce591a301c2275cae161458862ad52385f
taehee <jangth0655@gmail.com>
2022-11-27 23:26:34 +0900

    [feat] detail page  서버사이드 적용

--- components/NextSEO.tsx
+++ components/NextSEO.tsx
@@ -1,52 +1,29 @@
-//import { NextSeo } from 'next-seo';
-import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 import { useCarItem } from '../hooks/useCarList';
+import { Attribute } from '../model/types';
 
 import formatter from '../utils/formatter';
 
-const NextSEO = () => {
-  const { car } = useCarItem();
+interface Props {
+  attribute: Attribute;
+  amount: number;
+  id: string;
+}
 
+const NextSEO = ({ amount, attribute, id }: Props) => {
   return (
-    <Head>
-      <title>{`${car?.attribute.brand} ${car?.attribute.name}`}</title>
-      <meta name='description' content={`월 ${car?.amount}원`} />
-      <meta property='og:type' content='website' />
-      <link href={car?.attribute.imageUrl} />
-      <meta
-        property='og:url'
-        content={`${process.env.PUBLIC_URL}/${car?.id}`}
-      />
-      <meta
-        name='og:title'
-        content={`${car?.attribute.brand} ${car?.attribute.name}`}
-      />
-      <meta
-        name='og:description'
-        content={formatter.convertCurrency(car?.amount)}
-      />
-      <meta property='og:image' content={car?.attribute.imageUrl} />
-      <meta property='og:image:width' content={1200 + ''} />
-      <meta property='og:image:height' content={600 + ''} />
-    </Head>
-  );
-};
-
-export default NextSEO;
-
-/**
- <NextSeo
-      title={title ? title : 'BC2 차량서비스'}
-      description={formatter.convertCurrency(car?.amount)}
+    <NextSeo
+      title={`${attribute.name} ${attribute.brand}`}
+      description={formatter.convertCurrency(amount)}
       openGraph={{
         type: 'website',
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/${car?.id}`,
-        title: `${car?.attribute?.brand} ${car?.attribute?.name}`,
-        description: formatter.convertCurrency(car?.amount),
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/${id}`,
+        title: `${attribute?.brand} ${attribute?.name}`,
+        description: formatter.convertCurrency(amount),
         images: [
           {
-            url: car?.attribute?.imageUrl
-              ? car?.attribute?.imageUrl
+            url: attribute?.imageUrl
+              ? attribute?.imageUrl
               : '/public/favicon.ico',
             width: 1200,
             height: 600,
@@ -55,4 +32,7 @@ export default NextSEO;
         ],
       }}
     />
- */
+  );
+};
+
+export default NextSEO;
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,14 +1,37 @@
-import { NextPage } from 'next';
+import { NextPage, NextPageContext } from 'next';
 import CarDetail from '../components/carDetail/CarDetail';
 import NextSEO from '../components/NextSEO';
+import { CarList, CarListType } from '../model/types';
 
-const Detail: NextPage = () => {
+const Detail: NextPage<{ carItem: CarListType; id: any }> = ({
+  carItem,
+  id,
+}) => {
   return (
     <>
-      <NextSEO />
+      <NextSEO
+        amount={carItem?.amount}
+        attribute={carItem?.attribute}
+        id={id}
+      />
       <CarDetail />
     </>
   );
+};
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const id = context.query.id;
+  const res: CarList = await (
+    await fetch('https://preonboarding.platdev.net/api/cars')
+  ).json();
+  const car = res?.payload.find((car) => car.id === Number(id));
+
+  return {
+    props: {
+      carItem: car,
+      id,
+    },
+  };
 };
 
 export default Detail;
